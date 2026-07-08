78df8bc02c43395e59674f8bd074cf3c43cb9a9e
kachuchuru <kachuchuru@yahoo.com>
2026-07-08 23:16:03 +0300

    Complete Week01 Exrecise4

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,78 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        //Requirement 1
+        //Console.Write("What is the magic number? ");
+        //string magicNumber =Console.ReadLine();
+        //int x =int.Parse(magicNumber);
+        //Console.Write("What is your guess?  ");
+        //string guess = Console.ReadLine();
+        //int y =int.Parse(guess);
+        //if (x > y)
+        {
+            Console.WriteLine("Higher");
+        }
+        //else if (x < y)
+        {
+            Console.WriteLine("Lower");
+        }
+        //else
+        {
+            Console.WriteLine("You guessed it");
+        }
+        //Requirement2
+     Console.Write("What is the magic number? ");
+     string magikNumber =Console.ReadLine();
+     int x =int.Parse(magikNumber);
+        
+      x = 7;
+     int y = -1;
+     while (y!=x)
+
+     {
+        Console.Write("What is your guess?  ");
+        int guess = int.Parse(Console.ReadLine());
+        
+        if (y < x)
+        {
+            Console.WriteLine("Higher");
+        }
+        else if (y > x)
+        {
+            Console.WriteLine("Lower");
+        }
+     }    
+        
+        Console.WriteLine("You guessed it");
+      
+      // Requirement 3
+       Random randomGenerator=new Random();
+       int magicNumber = randomGenerator.Next(1,101);
+       int guess =-1;
+       while(guess!=magicNumber)
+       {
+            Console.Write(" What is your guess");
+            guess=int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess)
+            {
+               Console.WriteLine("Higher");
+            }
+            else if (magicNumber < guess)
+            {
+               Console.WriteLine("Lower");
+            }
+            else
+            {
+            Console.WriteLine(" You guessed it");
+            }
+
+        }
     }
 }
--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,48 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise4 Project.");
+
+
+       List<int> numbers=new List<int>();
+       int number= -1;
+       int sum = 0;
+       int count = 0;
+       double average = 0.0;
+       
+         Console.WriteLine("Enter a list of numbers, type 0 when finished");
+       while(number!=0)
+        {
+            
+            Console.Write("Enter the number ");
+            string userInput=Console.ReadLine();
+            number =int.Parse(userInput);
+            numbers.Add(number);
+            sum+=number;
+            count++;
+            
+            
+        }
+        int largest = numbers[0];
+        foreach (int value in numbers)  
+        {
+            if (value > largest)
+            {
+                largest = value;
+            }
+        }   
+            Console.WriteLine($"The Sum is: {sum}");
+            average=(double)sum/(count-1);
+            Console.WriteLine($"The average is: {average}");
+            Console.WriteLine($"The largest number is: {largest}");
+
+        
+
+
     }
 }
