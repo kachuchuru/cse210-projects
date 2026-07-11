f1cf64a05d4dc819bf18cd2c846040401877a83a
kachuchuru <kachuchuru@yahoo.com>
2026-07-12 02:54:38 +0300

    Complete Week 02 Resume assignment

---
+++ week02/Resumes/Job.cs
@@ -0,0 +1,15 @@
+
+
+public class Job
+{
+    public string _companyName="";
+    public string _jobTitle="";
+    public int _startYear=0;
+    public int _endYear=0;
+    public void Display()
+    {
+        Console.WriteLine($"{_jobTitle} ({_companyName}) {_startYear}-{_endYear}");
+    }
+
+
+}
