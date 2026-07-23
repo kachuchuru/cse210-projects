d52e6712915206d92c34727e54cd1927ad2bc9d2
kachuchuru <kachuchuru@yahoo.com>
2026-07-23 13:55:27 +0300

    Completed creation of files with stubs for the member classes

---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,30 @@
+
+using System;
+using System.Numerics;
+
+public class Scripture
+{
+  private Reference _reference;
+  private List<Word> _words;
+
+  public Scripture (Reference reference, string text)
+  {
+
+  }
+  public void HideRandomWords()
+{
+    
+    return;
+}  
+  public string GetDisplayText()
+{
+    string text="";
+  return text;
+}
+  public bool IsCompletelyHidden()
+
+{
+   
+   return false;
+}
+}
---
+++ week03/ScriptureMemorizer/Word.cs
@@ -0,0 +1,35 @@
+
+using System;
+using System.ComponentModel;
+public class Word
+{
+    private string _text;
+    private bool _string;
+
+    public Word(string text)
+    {
+        
+    }
+
+    public void Hide()
+    {
+        
+    }
+
+    public void Show()
+    {
+        
+    }
+
+    public bool IsHidden()
+    {
+
+      return true; 
+    }
+
+    public string GetDisplayText()
+    {
+       string text= "";
+       return text;
+    }
+}
