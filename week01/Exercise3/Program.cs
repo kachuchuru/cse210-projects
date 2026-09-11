d07452fd0830847173c5bedc8b8da4d41664aa2e
kachuchuru <kachuchuru@yahoo.com>
2026-09-12 02:13:51 +0300

    Made changes on Exercise3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,76 +5,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
 
         //Requirement 1
-        //Console.Write("What is the magic number? ");
-        //string magicNumber =Console.ReadLine();
-        //int x =int.Parse(magicNumber);
-        //Console.Write("What is your guess?  ");
-        //string guess = Console.ReadLine();
-        //int y =int.Parse(guess);
-        //if (x > y)
-        {
-            Console.WriteLine("Higher");
-        }
-        //else if (x < y)
-        {
-            Console.WriteLine("Lower");
-        }
-        //else
-        {
-            Console.WriteLine("You guessed it");
-        }
+         //Console.Write("What is the magic number? ");
+        //int magicNumber = int.Parse(Console.ReadLine());
+        
+    
         //Requirement2
-     //Console.Write("What is the magic number? ");
-     //string magikNumber =Console.ReadLine();
-     //int x =int.Parse(magikNumber);
-        
-     
-     //int y = -1;
-     //while (y!=x)
-
-     {
-        //Console.Write("What is your guess?  ");
-         //y= int.Parse(Console.ReadLine());
-        
-        //if (y < x)
-        {
-            //Console.WriteLine("Higher");
-        }
-        //else if (y > x)
-        {
-            Console.WriteLine("Lower");
-        }
-        //else
-        {
-            //Console.WriteLine("You guessed it");
-        }
-     }
-        
+        //Console.Write("What is the magic number? ");
+        //int magicNumber =int.Parse(Console.ReadLine());
+      
 
       
-      // Requirement 3
+        // Requirement 3
        Random randomGenerator=new Random();
        int magicNumber = randomGenerator.Next(1,101);
        int guess =-1;
        while(guess!=magicNumber)
        {
-            Console.Write(" What is your guess");
-            guess=int.Parse(Console.ReadLine());
+         Console.Write(" What is your guess?" );
+         guess=int.Parse(Console.ReadLine());
 
-            if (magicNumber > guess)
+         if (magicNumber > guess)
             {
                Console.WriteLine("Higher");
             }
-            else if (magicNumber < guess)
+         else if (magicNumber < guess)
             {
                Console.WriteLine("Lower");
             }
-            else
+         else
             {
-            Console.WriteLine(" You guessed it");
+                Console.WriteLine(" You guessed it");
             }
 
         }
