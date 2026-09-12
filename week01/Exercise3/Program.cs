af1576c41ab3221e15fd736e8f7e58de7366dfb4
kachuchuru <kachuchuru@yahoo.com>
2026-09-12 14:41:22 +0300

    Made corrections on Exercise3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -7,16 +7,49 @@ class Program
     {
 
         //Requirement 1
-         //Console.Write("What is the magic number? ");
-        //int magicNumber = int.Parse(Console.ReadLine());
-        
-    
+          
+         // Console.Write("What is the magic number? ");
+          //int magicNumber = int.Parse(Console.ReadLine());
+          
+          //Console.Write("What is your guess? ");
+          //int guess=int.Parse(Console.ReadLine());
+    //if (magicNumber > guess)
+            {
+               //Console.WriteLine("Higher");
+            }
+         //else if (magicNumber < guess)
+            {
+              // Console.WriteLine("Lower");
+            }
+         //else
+            {
+               // Console.WriteLine(" You guessed it");
+            }
         //Requirement2
+         //int guess=-1;
+         //int magicNumber=-1;
+        
         //Console.Write("What is the magic number? ");
-        //int magicNumber =int.Parse(Console.ReadLine());
-      
-
-      
+        //magicNumber =int.Parse(Console.ReadLine());
+        
+        //while (guess!=magicNumber)
+        {
+        //Console.Write("What is your guess? ");
+        //guess =int.Parse(Console.ReadLine());
+        
+        //if (magicNumber > guess)
+            {
+               //Console.WriteLine("Higher");
+            }
+         //else if (magicNumber < guess)
+            {
+               //Console.WriteLine("Lower");
+            }
+         //else
+            {
+                //Console.WriteLine(" You guessed it");
+            }
+        }
         // Requirement 3
        Random randomGenerator=new Random();
        int magicNumber = randomGenerator.Next(1,101);
